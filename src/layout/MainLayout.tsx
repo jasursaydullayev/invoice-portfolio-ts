6c7f77afb65b712dfa7936f31e4a01044840ac28
Jasurbek1810 <JasurbekSaydullayev@gmail.com>
2024-04-09 19:00:28 +0500

    ./

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,14 +1,11 @@
 import { Outlet, useParams } from "react-router-dom";
 import Navbar from "./Navbar";
-// import Footer from "./Footer";
 
 function MainLayout() {
-  // const params = useParams()
   return (
     <div className="flex h-full 1285:flex-col 1285:w-full overflow-scroll overflow-x-hidden">
       <Navbar />
       <Outlet /> 
-      {/* {params.id ? <Footer /> : ""} */}
     </div>
   );
 }
